eb467e1ad1c2e5efbc9c11abd49c6b6bd41e5d2d
Mahfuzul Haque <mahfuzul.haque@icarehealth.com>
2015-12-25 21:34:02 +1100

    Addred helptext

--- cvd-risk-calculator/ui.R
+++ cvd-risk-calculator/ui.R
@@ -6,6 +6,7 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
+      helpText("This app predicts the risk of cardiovascular disease (CVD) of an individual based on following demographic, lifestyle, and health information. The predicted risk (%) is displayed on the right hand side panel."),
       numericInput("age", "Enter age", min = 20, max = 100, value = 50),
       selectInput("gender", "Gender", choices = list("Male" = 0, "Female" = 1), selected = 0),
       selectInput("smoker", "Is smoker?", choices = list("No" = 0, "Yes" = 1), selected = 0),
@@ -18,6 +19,7 @@ shinyUI(fluidPage(
     mainPanel(
       htmlOutput("risk"),
       hr(),
+      helpText("The following graph shows the level of risk at different ages assuming other inputs constant."),
       plotOutput("plotRisk")
     )
   )
